165e3434d7ff1e80aa711c9a2db946343fb0871b
elystanz <elysestanziale@gmail.com>
2022-07-15 20:38:25 -0400

    finish idb js

--- public/idb.js
+++ public/idb.js
@@ -1,4 +1,7 @@
+// set variables
 let db;
+
+// create a budget DB request
 const req = indexedDB.open('budget', 1);
 
 req.onupgradeneeded = function (event) {
@@ -18,3 +21,46 @@ req.onerror = function (event) {
     console.log(event.target.error);
 };
 
+// open a transaction, get all objects
+function checkDb() {
+    const db = request.result;
+
+    let transaction = db.transaction('pendingTransac', 'readwrite');
+    const store = transaction.objectStore('pendingTransac');
+
+    // set all objects to getAll variable
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers : {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                },
+            })
+
+            // on success, access pending transactions
+            .then((response) => response.json())
+            .then(() => {
+                const transaction = db.transaction(['pendingTransac'], 'readwrite');
+                const store = transaction.objectStore('pendingTransac');
+
+                store.clear();
+            });
+        };
+    };
+
+
+// save transaction object
+    function saveRecord(record) {
+        const transaction = db.transaction('pendingTransac', 'readwrite');
+        const store = transaction.objectStore('pendingTransac');
+
+        store.add(record);
+    }
+};
+
+window.addEventListener('online', checkDb);
